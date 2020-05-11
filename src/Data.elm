b1b552181401de00415f6c63659438656904a664
willis plummer <willisplummer@gmail.com>
2020-05-10 20:39:21 -0400

    remainder

--- src/Data.elm
+++ src/Data.elm
@@ -8,7 +8,8 @@ reading : ReadingList
 reading =
     fromList
         [ ( 2020
-          , [ ( "days", "mary robison" )
+          , [ ( "remainder", "tom mccarthy" )
+            , ( "days", "mary robison" )
             , ( "77 dream songs", "john berryman" )
             , ( "preliminary materials for a theory of the young-girl", "tiqqun" )
             , ( "hamburger in the archive", "jen calleja" )
