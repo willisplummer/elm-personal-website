9ae82bac2e774c981bcc8adfaf830c948b5210b8
willis plummer <willisplummer@gmail.com>
2020-09-03 14:19:21 -0400

    arkansas

--- src/Data.elm
+++ src/Data.elm
@@ -8,7 +8,8 @@ reading : ReadingList
 reading =
     fromList
         [ ( 2020
-          , [ ( "beauty was the case that they gave me (reread)", "mark leidner" )
+          , [ ( "arkansas", "john brandon" )
+            , ( "beauty was the case that they gave me (reread)", "mark leidner" )
             , ( "100 poems from the chinese", "kenneth rexroth" )
             , ( "leave society (second draft)", "tao lin" )
             , ( "king of a hundred horsemen", "marie etienne" )
